21c6f062889133c21e8778cd3da9d46452fdca25
chenglezeng <chenglezeng@tencent.com>
2020-12-03 20:51:54 +0800

    feat: 去除立即执行函数的包括，因为会导致难以定位的异常

---
+++ src/index.js
@@ -0,0 +1,7 @@
+const spy = require('./spy');
+const mock = require('./mock');
+
+module.exports = {
+  spy,
+  mock,
+};
--- src/wrapper.js
+++ src/wrapper.js
@@ -1,104 +1,104 @@
 /* eslint-disable no-param-reassign */
-(function () {
-  const flagMap = new Map();
+const $tfmFlagMap = new Map();
 
-  const mockKey = '__tfmMock';
-  const spyFlagKey = '__tfmSpyFlag';
-  const spyInfoKey = '__tfmSpyInfo';
+const $tfmMockKey = '__tfmMock';
+const $tfmSpyFlagKey = '__tfmSpyFlag';
+const $tfmSpyInfoKey = '__tfmSpyInfo';
 
-  function isFunction(target) {
-    return typeof target === 'function';
-  }
+function $tfmIsFunction(target) {
+  return typeof target === 'function';
+}
 
-  function isObject(target) {
-    return typeof target === 'object' && target !== null;
-  }
+function $tfmIsObject(target) {
+  return typeof target === 'object' && target !== null;
+}
 
-  function getInitSpyInfo(target) {
-    return {
-      called: true,
-      callCount: 0,
-      callArgs: [],
-      lastCallArgs: [],
-      returnValues: [],
-      lastReturnValue: undefined,
-      restore() {
-        flagMap.delete(target);
-        delete target[spyInfoKey];
-        delete target[mockKey];
-      },
-    };
-  }
+function $tfmGetInitSpyInfo(target) {
+  return {
+    called: true,
+    callCount: 0,
+    callArgs: [],
+    lastCallArgs: [],
+    returnValues: [],
+    lastReturnValue: undefined,
+    restore() {
+      $tfmFlagMap.delete(target);
+      delete target[$tfmSpyInfoKey];
+      delete target[$tfmMockKey];
+    },
+  };
+}
 
-  function wrapFunc(func) {
-    return new Proxy(func, {
-      apply(target, thisArg, args) {
-        const mockFunc = target[mockKey];
-        const funcToCall = mockFunc || target;
-
-        const returnValue = funcToCall.apply(thisArg, args);
-        const isSpied = flagMap.get(target);
-        if (isSpied) {
-          const spyInfo = target[spyInfoKey];
-          spyInfo.callCount += 1;
-          spyInfo.callArgs.push(args);
-          spyInfo.lastCallArgs = args;
-          spyInfo.returnValues.push(returnValue);
-          spyInfo.lastReturnValue = returnValue;
-        }
-
-        return returnValue;
-      },
-      set(target, prop, value) {
-        if (prop === spyFlagKey && value === true) {
-          flagMap.set(target, true);
-          Object.defineProperty(target, spyInfoKey, {
-            value: getInitSpyInfo(target),
-            configurable: true,
-          });
-        } else if (prop === mockKey && isFunction(value)) {
-          flagMap.set(target, true);
-          Object.defineProperty(target, mockKey, {
-            value,
-            configurable: true,
-          });
-          Object.defineProperty(target, spyInfoKey, {
-            value: getInitSpyInfo(target),
-            configurable: true,
-          });
-        } else {
-          return Reflect.set(target, prop, value);
-        }
-      },
-    });
-  }
-
-  const MAX_DEPTH = 5; // 最多递归5层
-  function wrapFuncByRecurse(target, depth) {
-    depth = depth || 1;
-    if (depth > MAX_DEPTH) {
-      return;
-    }
-    const keys = Object.keys(target);
-    let key;
-    for (let i = 0; i < keys.length; i += 1) {
-      key = keys[i];
-      if (isFunction(target[key])) {
-        Object.defineProperty(target, key, {
-          value: wrapFunc(target[key]),
-        });
-        wrapFuncByRecurse(target[key], depth + 1);
-      } else if (isObject(target[key])) {
-        wrapFuncByRecurse(target[key], depth + 1);
+function $tfmWrapFunc(func) {
+  return new Proxy(func, {
+    apply(target, thisArg, args) {
+      const mockFunc = target[$tfmMockKey];
+      const funcToCall = mockFunc || target;
+      const returnValue = funcToCall.apply(thisArg, args);
+      const isSpied = $tfmFlagMap.get(target);
+      if (isSpied) {
+        const spyInfo = target[$tfmSpyInfoKey];
+        spyInfo.callCount += 1;
+        spyInfo.callArgs.push(args);
+        spyInfo.lastCallArgs = args;
+        spyInfo.returnValues.push(returnValue);
+        spyInfo.lastReturnValue = returnValue;
       }
+
+      return returnValue;
+    },
+    set(target, prop, value) {
+      if (prop === $tfmSpyFlagKey && value === true) {
+        $tfmFlagMap.set(target, true);
+        Object.defineProperty(target, $tfmSpyInfoKey, {
+          value: $tfmGetInitSpyInfo(target),
+          configurable: true,
+        });
+      } else if (prop === $tfmMockKey && $tfmIsFunction(value)) {
+        $tfmFlagMap.set(target, true);
+        Object.defineProperty(target, $tfmMockKey, {
+          value,
+          configurable: true,
+        });
+        Object.defineProperty(target, $tfmSpyInfoKey, {
+          value: $tfmGetInitSpyInfo(target),
+          configurable: true,
+        });
+      } else {
+        return Reflect.set(target, prop, value);
+      }
+    },
+  });
+}
+
+const $TFM_MAX_DEPTH = 5; // 最多递归5层
+function $tfmWrapFuncByRecurse(target, depth) {
+  depth = depth || 1;
+  if (depth > $TFM_MAX_DEPTH) {
+    return;
+  }
+  const keys = Object.keys(target);
+  let key;
+  for (let i = 0; i < keys.length; i += 1) {
+    key = keys[i];
+    if ($tfmIsFunction(target[key])) {
+      const { configurable } = Object.getOwnPropertyDescriptor(target, key);
+      if (configurable) {
+        Object.defineProperty(target, key, {
+          value: $tfmWrapFunc(target[key]),
+        });
+      }
+      $tfmWrapFuncByRecurse(target[key], depth + 1);
+    } else if ($tfmIsObject(target[key])) {
+      $tfmWrapFuncByRecurse(target[key], depth + 1);
     }
   }
+}
 
-  const output = module.exports;
-  if (isFunction(output)) {
-    wrapFuncByRecurse(output);
-    module.exports = wrapFunc(output);
-  } else if (isObject(output)) {
-    wrapFuncByRecurse(output);
-  }
-}());
+const $tfmModuleExports = module.exports;
+if ($tfmIsFunction($tfmModuleExports)) {
+  $tfmWrapFuncByRecurse($tfmModuleExports);
+  module.exports = $tfmWrapFunc($tfmModuleExports);
+} else if ($tfmIsObject($tfmModuleExports)) {
+  $tfmWrapFuncByRecurse($tfmModuleExports);
+}
