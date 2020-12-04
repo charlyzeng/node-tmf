f6d7f64585a34282027dbb29fbebf196835124f4
chenglezeng <chenglezeng@tencent.com>
2020-12-04 10:42:06 +0800

    test: 补充mock默认值相关单测

---
+++ test/generator.js
@@ -0,0 +1,5 @@
+module.exports = function* () {
+  yield 1;
+  yield 2;
+  return 3;
+};
--- test/mock.test.js
+++ test/mock.test.js
@@ -2,6 +2,8 @@ const assert = require('power-assert');
 const sum = require('./mod1');
 const { mul } = sum;
 const { mock } = require('../src');
+const asyncFunc = require('./async');
+const generatorFunc = require('./generator');
 
 describe('mock test', () => {
   it('mock for module when exports is function', () => {
@@ -38,5 +40,56 @@ describe('mock test', () => {
 
     assert.equal(mul(1, 2), 2);
     assert.equal(mul(3, 4), 12);
+  });
+
+  it('mock should have an right default func', async () => {
+    assert.equal(sum(1, 2), 3);
+    const mockSum = mock(sum);
+    assert.equal(sum(1, 2), undefined);
+    mockSum.restore();
+    assert.equal(sum(1, 2), 3);
+
+    assert.equal(await asyncFunc(), 'async');
+    const mockAsyncFunc = mock(asyncFunc);
+    const asyncResult = asyncFunc();
+    const asyncValue = await asyncResult;
+    assert.ok(asyncResult, Promise);
+    assert.equal(asyncValue, undefined);
+    mockAsyncFunc.restore();
+    assert.equal(await asyncFunc(), 'async');
+
+    let originGeneratorResult = generatorFunc();
+    assert.deepEqual(originGeneratorResult.next(), {
+      done: false,
+      value: 1,
+    });
+    assert.deepEqual(originGeneratorResult.next(), {
+      done: false,
+      value: 2,
+    });
+    assert.deepEqual(originGeneratorResult.next(), {
+      done: true,
+      value: 3,
+    });
+    const mockGeneratorFunc = mock(generatorFunc);
+    const generatorResult = generatorFunc();
+    assert.deepEqual(generatorResult.next(), {
+      done: true,
+      value: undefined,
+    });
+    mockGeneratorFunc.restore();
+    originGeneratorResult = generatorFunc();
+    assert.deepEqual(originGeneratorResult.next(), {
+      done: false,
+      value: 1,
+    });
+    assert.deepEqual(originGeneratorResult.next(), {
+      done: false,
+      value: 2,
+    });
+    assert.deepEqual(originGeneratorResult.next(), {
+      done: true,
+      value: 3,
+    });
   });
 });
